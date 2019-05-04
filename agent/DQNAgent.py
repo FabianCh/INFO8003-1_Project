2fbc55a59bda19326c207713846a5a4a27b5ea24
FabianCh <fabianchauvin@gmail.com>
2019-05-04 15:07:33 +0200

    Implementation of DQN

---
+++ agent/DQNAgent.py
@@ -0,0 +1,63 @@
+from agent.agent import Agent
+from policy.random_policy import RandomPolicy
+from policy.greedy_policy import GreedyPolicy
+from estimator.neural_network import NeuralNetworkEstimator
+
+
+class DQNAgent(Agent):
+
+    def __init__(self, buffer, maximazer):
+        super(DQNAgent, self).__init__(buffer, NeuralNetworkEstimator, maximazer)
+
+        self.Q = self.estimator()
+        self.learning_ratio = 0.05
+
+    def PlayDQN(self, T):
+        self.domain.reset()
+        policy = RandomPolicy()
+
+        state, is_terminate = self.domain.observe(), False
+
+
+
+        t = 1
+        while t < T:
+            if is_terminate:
+                self.domain.reset()
+                state = self.domain.observe()
+                print('end')
+
+            action = policy(state)
+            next_state, reward, is_terminate = self.domain.step(action)
+            if reward == 3:
+                print('hit')
+
+            one_step_transition = (state, action, reward, next_state)
+            self.buffer.add_sample(one_step_transition)
+
+            self.train(1)
+
+            state = next_state
+            t += 1
+            # print(t)
+
+    def train(self, depth=1, mini_batch=30):
+        list_ost = self.buffer.get_sample(mini_batch)
+
+        train_x = []
+        train_y = []
+
+        for state, action, reward, next_state in list_ost:
+            if reward == 3:
+                print("    Learn From Hit")
+            x = [state[0], state[1], state[2], state[3], action[0], action[1]]
+            x_prime = [next_state[0], next_state[1], next_state[2], next_state[3]]
+            y = reward + \
+                self.gamma * self.maximizer.value(self.Q, x_prime)
+            train_x.append(x)
+            train_y.append(y)
+
+        self.Q.train(train_x, train_y)
+
+    def get_optimal_policy(self):
+        return GreedyPolicy(self.Q, self.maximizer, 0.1)
