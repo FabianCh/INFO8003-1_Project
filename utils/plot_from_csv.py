07b41648a7f49a16ce1b8c753f803ac962ca1794
FabianCh <fabianchauvin@gmail.com>
2019-05-15 21:22:20 +0200

    Add the bias on graph

--- utils/plot_from_csv.py
+++ utils/plot_from_csv.py
@@ -2,40 +2,65 @@ import csv
 import matplotlib.pyplot as plt
 
 y_er_static, y_er_uniform, y_er_balanced = [], [], []
+y_er_bias_static, y_er_bias_uniform, y_er_bias_balanced = [], [], []
 y_h_static, y_h_uniform, y_h_balanced = [], [], []
+y_h_bias_static, y_h_bias_uniform, y_h_bias_balanced = [], [], []
+esup, eund = [], []
 
-with open("output/evaluation - Copie/Train--initialbuffersize_50000-episode_100-episode_size_1000-minibatchsize_30-decreaserate_0.00001-targetupdate_10000-Agent--agent_DDQNAgent-estimator_NeuralNetwork-buffer_Prioritized-maximizer_Static-Static--numberofsample_11-_log.csv",
+import os
+print(os.getcwd())
+
+with open("../output/evaluation2/Static150.csv",
           'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
         y_er_static.append(float(row[0]))
-        y_h_static.append(float(row[1]))
+        y_er_bias_static.append(float(row[1])**(1/2))
+        y_h_static.append(float(row[2]))
+        y_h_bias_static.append(float(row[3])**(1/2))
 
-with open("output/evaluation - Copie/Train--initialbuffersize_50000-episode_100-episode_size_1000-minibatchsize_30-decreaserate_0.00001-targetupdate_10000-Agent--agent_DDQNAgent-estimator_NeuralNetwork-buffer_Prioritized-maximizer_Uniform-Uniform--numberofsample_11-_log.csv",
+with open("../output/evaluation2/U150.csv",
           'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
         y_er_uniform.append(float(row[0]))
-        y_h_uniform.append(float(row[1]))
+        y_er_bias_uniform.append(float(row[1])**(1/2))
+        y_h_uniform.append(float(row[2]))
+        y_h_bias_uniform.append(float(row[3])**(1/2))
+        esup.append(float(row[2])+(float(row[3])**(1/2)))
+        eund.append(float(row[2]) - (float(row[3]) ** (1 / 2)))
 
-with open("output/evaluation - Copie/Train--initialbuffersize_50000-episode_100-episode_size_1000-minibatchsize_30-decreaserate_0.00001-targetupdate_10000-Agent--agent_DDQNAgent-estimator_NeuralNetwork-buffer_Prioritized-maximizer_BalancedUniform-BalancedUniform--numberofsample_5-_log.csv",
+with open("../output/evaluation2/BU150_2.csv",
           'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
         y_er_balanced.append(float(row[0]))
-        y_h_balanced.append(float(row[1]))
+        y_er_bias_balanced.append(float(row[1])**(1/2))
+        y_h_balanced.append(float(row[2]))
+        y_h_bias_balanced.append(float(row[3])**(1/2))
 
-plt.plot([i for i in range(len(y_er_static))], y_er_static, 'b', label='Static Sampler')
-plt.plot([i for i in range(len(y_er_uniform))], y_er_uniform, 'r', label='Uniform Sampler')
-plt.plot([i for i in range(len(y_er_balanced))], y_er_balanced, 'g', label='Balanced Uniform Sampler')
+plt.plot([i for i in range(len(y_er_static))], y_er_static, '#362893', label='Static Sampler')
+plt.plot([i for i in range(len(y_er_uniform))], y_er_uniform, '#b32d30', label='Uniform Sampler')
+plt.plot([i for i in range(len(y_er_balanced))], y_er_balanced, '#4d7e2b', label='Balanced Uniform Sampler')
+
+plt.errorbar([i for i in range(len(y_er_static))], y_er_static, y_er_bias_static, fmt='none',  ecolor='#a0bbff', label='Static Sampler error' )
+plt.errorbar([i for i in range(len(y_er_uniform))], y_er_uniform, y_er_bias_uniform, fmt='none', ecolor='#ffa8a8', label='Uniform Sampler error')
+plt.errorbar([i for i in range(len(y_er_balanced))], y_er_balanced, y_er_bias_balanced, fmt='none', ecolor='#9cff58', label='Balanced Uniform Sampler error')
+
 plt.legend(loc="upper left")
 plt.xlabel("Episodes")
 plt.ylabel("Expected Return")
 plt.show()
 
-plt.plot([i for i in range(len(y_h_static))], y_h_static, 'b', label='Static Sampler')
-plt.plot([i for i in range(len(y_h_uniform))], y_h_uniform, 'r', label='Uniform Sampler')
-plt.plot([i for i in range(len(y_h_balanced))], y_h_balanced, 'g', label='Balanced Uniform Sampler')
+
+plt.plot([i for i in range(len(y_h_static))], y_h_static, '#362893', label='Static Sampler')
+plt.plot([i for i in range(len(y_h_uniform))], y_h_uniform, '#b32d30', label='Uniform Sampler')
+plt.plot([i for i in range(len(y_h_balanced))], y_h_balanced, '#4d7e2b', label='Balanced Uniform Sampler')
+
+plt.errorbar([i for i in range(len(y_h_static))], y_h_static, y_h_bias_static, fmt='none',  ecolor='#a0bbff', label='Static Sampler error' )
+plt.errorbar([i for i in range(len(y_h_uniform))], y_h_uniform, y_h_bias_uniform, fmt='none', ecolor='#ffa8a8', label='Uniform Sampler error')
+plt.errorbar([i for i in range(len(y_h_balanced))], y_h_balanced, y_h_bias_balanced, fmt='none', ecolor='#9cff58', label='Balanced Uniform Sampler error')
+
 plt.legend(loc="upper left")
 plt.xlabel("Episodes")
 plt.ylabel("Number of hits")
